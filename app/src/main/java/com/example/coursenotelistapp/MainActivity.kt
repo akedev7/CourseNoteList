cdd5fb67522d08268459268d7ff2490a2b026cdf
ake.tan <ake.tan@ascendcorp.com>
2021-12-05 10:15:47 +0700

    14 hours passed - Refactor code

--- app/src/main/java/com/example/coursenotelistapp/MainActivity.kt
+++ app/src/main/java/com/example/coursenotelistapp/MainActivity.kt
@@ -1,29 +1,25 @@
 package com.example.coursenotelistapp
 
-import android.R
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import com.example.coursenotelistapp.databinding.ActivityMainBinding
-import com.example.myfirstapplication.CourseInfo
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myfirstapplication.DataManager
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         val dm = DataManager()
-        val adapterCourses = ArrayAdapter<CourseInfo>(
+        val adapterCourses = ArrayAdapter(
             this,
-            R.layout.simple_spinner_item,
+            android.R.layout.simple_spinner_item,
             dm.courses.values.toList()
         )
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerCourses.adapter = adapterCourses
+
+        val spinnerCourse = findViewById<Spinner>(R.id.spinnerCourses)
+        spinnerCourse.adapter = adapterCourses
     }
 }
