767aab78ceb278576fd1552e86ad83e7cada25e7
ake.tan <ake.tan@ascendcorp.com>
2021-12-05 14:18:04 +0700

    15 hours passed - Update app bar at runtime by changing app bar icon to blocking icon when it reach the last index of the list

--- app/src/main/java/com/example/coursenotelistapp/MainActivity.kt
+++ app/src/main/java/com/example/coursenotelistapp/MainActivity.kt
@@ -63,5 +63,17 @@ class MainActivity : AppCompatActivity() {
     private fun moveNext() {
         ++notePosition
         displayNote()
+        invalidateOptionsMenu() // after call this method, onPrepareOptionsMenu will be called
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        if (notePosition >= DataManager.notes.lastIndex) {
+            val menuItem = menu?.findItem(R.id.action_next)
+            if (menuItem != null) {
+                menuItem.icon = getDrawable(R.drawable.ic_baseline_block_24)
+                menuItem.isEnabled = false
+            }
+        }
+        return super.onPrepareOptionsMenu(menu)
     }
 }
